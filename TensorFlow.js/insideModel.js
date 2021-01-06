787fc9cf23a02e165148b285f8d186f8388915cd
SunEom <dkxl0701@gmail.com>
2021-01-06 12:41:07 +0900

    Save tensor value

---
+++ TensorFlow.js/insideModel.js
@@ -0,0 +1,26 @@
+const tf = require('@tensorflow/tfjs');
+
+const temperature = [20, 21, 22, 23, 24];
+const salesVolume = [40, 42, 44, 46, 48];
+const cause = tf.tensor(temperature);
+const result = tf.tensor(salesVolume);
+
+const X = tf.input({ shape: [1] });
+const Y = tf.layers.dense({ units: 1 }).apply(X);
+const model = tf.model({ inputs: X, outputs: Y });
+const compileParam = { optimizer: tf.train.adam(), loss: tf.losses.meanSquaredError };
+model.compile(compileParam);
+
+const fitParam = {
+  epochs: 2000,
+  callbacks: {
+    onEpochEnd: (epoch, logs) => {
+      console.log('epoch', epoch, 'RMSE ->', Math.sqrt(logs.loss));
+    },
+  },
+};
+
+model.fit(cause, result, fitParam).then(async () => {
+  const predictedValues = await model.predict(cause).array();
+  console.log(predictedValues);
+});
