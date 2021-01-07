8cb818ba8b3b4283dc7da520b5fba60e56d48718
SunEom <dkxl0701@gmail.com>
2021-01-07 12:29:36 +0900

    Multiple dependent variables

---
+++ TensorFlow.js/housePriceSecond.js
@@ -0,0 +1,31 @@
+const tf = require('@tensorflow/tfjs');
+
+const { boston_cause, boston_result } = require('./housePriceDataSecond');
+
+const cause = tf.tensor(boston_cause);
+const result = tf.tensor(boston_result);
+
+// shape : 독립변수의 개수
+const X = tf.input({ shape: [12] });
+// units : 종속변수의 개수
+const Y = tf.layers.dense({ units: 2 }).apply(X);
+const model = tf.model({ inputs: X, outputs: Y });
+const compileParam = { optimizer: tf.train.adam(), loss: tf.losses.meanSquaredError };
+model.compile(compileParam);
+
+const fitParam = {
+  epochs: 3000,
+  callbacks: {
+    onEpochEnd: (epoch, logs) => {
+      console.log('epoch', epoch, 'RMSE => ', Math.sqrt(logs.loss));
+    },
+  },
+};
+model.fit(cause, result, fitParam).then(() => {
+  model.predict(cause).print();
+  const ws = model.getWeights();
+  const weight = ws[0];
+  const bias = ws[1];
+  weight.print();
+  bias.print();
+});
