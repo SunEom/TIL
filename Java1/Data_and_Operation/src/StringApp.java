c8fc753240cfdc03acefecf79d726a5e2c2f22c9
SunEom <dkxl0701@gmail.com>
2021-01-19 15:40:04 +0900

    String operation #1

---
+++ Java1/Data_and_Operation/src/StringApp.java
@@ -0,0 +1,21 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World"); // String
+		System.out.println('H'); // Character
+		System.out.println("H"); // String
+		
+		System.out.println("Hello "
+				+ "World");
+		
+		// new Line	
+		System.out.println("Hello \nWorld");
+		
+		// Hello "World"
+		// escape
+		System.out.println("Hello \"World\"");
+	}
+
+}
