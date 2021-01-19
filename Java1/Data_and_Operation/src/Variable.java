8267d2658da134be50250216e04210a52990d74a
SunEom <dkxl0701@gmail.com>
2021-01-19 15:59:04 +0900

    Variables

---
+++ Java1/Data_and_Operation/src/Variable.java
@@ -0,0 +1,17 @@
+
+public class Variable {
+
+	public static void main(String[] args) {
+		
+		int a = 1; //Number -> Integer
+		System.out.println(a);
+
+		double b = 1.1; // Number -> double
+		System.out.println(b);
+		
+		String c = "Hello World";
+		System.out.println(c);
+		
+	}
+
+}
