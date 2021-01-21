4d991106731bd79f365672aa9fb6764784fa0f0c
SunEom <dkxl0701@gmail.com>
2021-01-21 15:11:00 +0900

    Instance

--- Java_OOP/Project_OOP/MyOOP.java
+++ Java_OOP/Project_OOP/MyOOP.java
@@ -2,21 +2,61 @@ public class MyOOP {
 
 	public static void main(String[] args) {
 
-		Print.delimiter = "----";
-		//...
-		Print.A();
-		Print.A();
-		//...
-		Print.B();
-		Print.B();
+//		Print.delimiter = "----";
+//		//...
+//		Print.A();
+//		Print.A();
+//		//...
+//		Print.B();
+//		Print.B();
+//		
+//		Print.delimiter = "****";
+//		//...
+//		Print.A();
+//		Print.A();
+//		//...
+//		Print.B();
+//		Print.B();
 		
-		Print.delimiter = "****";
+		Print p1 = new Print();
+		p1.delimiter ="*****";
 		//...
-		Print.A();
-		Print.A();
+		p1.A();
+		p1.A();
 		//...
-		Print.B();
-		Print.B();
+		p1.B();
+		p1.B();
+		
+		Print p2 = new Print();
+		p2.delimiter ="*****";
+		//...
+		p2.A();
+		p2.A();
+		//...
+		p2.B();
+		p2.B();
+		
+
+//		Print.delimiter = "----";
+//		//...
+//		Print.A();
+		
+//		Print.delimiter = "****";
+//		//...
+//		Print.A();
+		
+//		Print.delimiter = "----";
+//		//...
+//		Print.A();
+		
+//		Print.delimiter = "****";
+//		//...
+//		Print.A();
+		
+		p1.A();
+		p2.A();
+		p1.A();
+		p2.A();
 		
 	}
 
