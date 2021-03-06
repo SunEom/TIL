8f09cbbf610918d5ee9927a1e88c06c9efe72828
SunEom <dkxl0701@gmail.com>
2021-03-06 20:43:46 +0900

    Chapter9 #02

--- DataStructure/Chapter9/huffman.c
+++ DataStructure/Chapter9/huffman.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #define MAX_ELEMENT 200
 
-typedef struct TreeNode{
+typedef struct TreeNode {
 	int weight;
 	char ch;
 	struct TreeNode* left;
@@ -10,9 +10,9 @@ typedef struct TreeNode{
 }TreeNode;
 
 typedef struct {
-	TreeNode* ptree;
-	char ch;
 	int key;
+	char ch;
+	TreeNode* ptree;
 }element;
 
 typedef struct {
@@ -21,7 +21,7 @@ typedef struct {
 }HeapType;
 
 HeapType* create() {
-	return  (HeapType*)malloc(sizeof(HeapType));
+	return (HeapType*)malloc(sizeof(HeapType));
 }
 
 void init(HeapType* h) {
@@ -30,7 +30,7 @@ void init(HeapType* h) {
 
 void insert_min_heap(HeapType* h, element item) {
 	int i;
-	i = ++(h->heap_size);
+	i = ++h->heap_size;
 
 	while (i != 1 && item.key < h->heap[i / 2].key) {
 		h->heap[i] = h->heap[i / 2];
@@ -48,7 +48,7 @@ element delete_min_heap(HeapType* h) {
 	parent = 1;
 	child = 2;
 	while (child <= h->heap_size) {
-		if (child<h->heap_size && h->heap[child].key > h->heap[child + 1].key)
+		if (child<h->heap_size && h->heap[child].key  > h->heap[child + 1].key)
 			child++;
 		if (temp.key < h->heap[child].key) break;
 		h->heap[parent] = h->heap[child];
@@ -60,21 +60,17 @@ element delete_min_heap(HeapType* h) {
 }
 
 TreeNode* make_tree(TreeNode* left, TreeNode* right) {
-	TreeNode* root = (TreeNode*)malloc(sizeof(TreeNode));
-	root->left = left;
-	root->right = right;
-	return root;
+	TreeNode* node = (TreeNode*)malloc(sizeof(TreeNode));
+	node->left = left;
+	node->right = right;
+	return node;
 }
 
-void distory_tree(TreeNode* root) {
+void destroy_tree(TreeNode* root) {
 	if (root == NULL) return;
-	distory_tree(root->left);
-	distory_tree(root->right);
+	destroy_tree(root->left);
+	destroy_tree(root->right);
 	free(root);
-}
-
-int is_leaf(TreeNode* root) {
-	return !root->left && !root->right;
 }
 
 void print_array(int codes[], int n) {
@@ -83,53 +79,57 @@ void print_array(int codes[], int n) {
 	printf("\n");
 }
 
-void printf_codes(TreeNode* root, int codes[], int top) {
+int is_leaf(TreeNode* node) {
+	return !node->left && !node->right;
+}
 
+void print_codes(TreeNode* root, int codes[], int top) {
 	if (root->left) {
 		codes[top] = 1;
-		printf_codes(root->left, codes, top + 1);
+		print_codes(root->left, codes, top + 1);
 	}
 	
 	if (root->right) {
 		codes[top] = 0;
-		printf_codes(root->right, codes, top + 1);
+		print_codes(root->right, codes, top + 1);
 	}
 
 	if (is_leaf(root)) {
-		printf("%c: ", root->ch);
+		printf("%c : ", root->ch);
 		print_array(codes, top);
 	}
 }
 
 void huffman_tree(int freq[], char ch_list[], int n) {
 	int i;
+	element e, e1, e2;
 	TreeNode* node, * x;
 	HeapType* heap;
-	element e, e1, e2;
 	int codes[100];
-	int top = 0;
+	int top=0;
 
 	heap = create();
 	init(heap);
-	for (i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++) {
 		node = make_tree(NULL, NULL);
 		e.ch = node->ch = ch_list[i];
 		e.key = node->weight = freq[i];
 		e.ptree = node;
 		insert_min_heap(heap, e);
 	}
-	for (i = 1; i < n; i++) {
+	for (int i = 1; i < n; i++) {
 		e1 = delete_min_heap(heap);
 		e2 = delete_min_heap(heap);
-		x = make_tree(&e1, &e2);
+
+		x = make_tree(e1.ptree, e2.ptree);
 		e.key = x->weight = e1.key + e2.key;
 		e.ptree = x;
 		printf("%d+%d->%d\n", e1.key, e2.key, e.key);
 		insert_min_heap(heap, e);
 	}
 	e = delete_min_heap(heap);
-	printf_codes(e.ptree, codes, top);
-	distory_tree(e.ptree);
+	print_codes(e.ptree, codes, top);
+	destroy_tree(e.ptree);
 	free(heap);
 }
 
