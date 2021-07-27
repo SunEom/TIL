6251f4416580025065ee34720483b77dab564dbb
SunEom <dkxl0701@gmail.com>
2021-07-27 12:55:03 +0900

    TIL : 2021.07.27.

--- Android/SampleActionBar1/app/src/main/java/org/techtown/actionbar/MainActivity.java
+++ Android/SampleActionBar1/app/src/main/java/org/techtown/actionbar/MainActivity.java
@@ -1,4 +1,4 @@
-package org.techtown.actionbar;
+package org.techtown.menu;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
@@ -10,6 +10,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import org.techtown.actionbar.R;
 
 public class MainActivity extends AppCompatActivity {
     ActionBar abar;
@@ -39,20 +41,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int curId = item.getItemId();
-        switch (curId){
+        switch(curId){
             case R.id.menu_refresh:
-                Toast.makeText(this, "새로고침 메뉴가 선택되었습니다.",Toast.LENGTH_LONG).show();
+                Toast.makeText(this,"새로고침 메뉴가 선택되었습니다.", Toast.LENGTH_LONG).show();
                 break;
             case R.id.menu_search:
-                Toast.makeText(this, "검색 메뉴가 선택되었습니다.",Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "검색 메뉴가 선택되었습니다.", Toast.LENGTH_LONG).show();
                 break;
             case R.id.menu_settings:
-                Toast.makeText(this, "설정 메뉴가 선택되었습니다.",Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "설정 메뉴가 선택되었습니다.", Toast.LENGTH_LONG).show();
                 break;
             default:
                 break;
         }
         return super.onOptionsItemSelected(item);
     }
-
 }
