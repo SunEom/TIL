b501c4fd943cec183bcdd1a5449d6ecb51610b59
SunEom <dkxl0701@gmail.com>
2021-07-28 14:06:24 +0900

    TIL : 2021.07.28.

---
+++ Android/SampleDrawer2/app/src/main/java/org/techtown/drawer/ui/Fragment1.java
@@ -0,0 +1,20 @@
+package org.techtown.drawer.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.techtown.drawer.R;
+
+public class Fragment1 extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment1, container, false);
+    }
+}
