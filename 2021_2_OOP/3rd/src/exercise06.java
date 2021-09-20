e3f272797af469b0c2d0f9d1367f39dd959e1817
SunEom <dkxl0701@gmail.com>
2021-09-20 16:40:09 +0900

    Array

---
+++ 2021_2_OOP/3rd/src/exercise06.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class exercise06 {
+   public static void main(String[] args){
+      double[] arr = {1.1, 2.3, 4.7, 7.5};
+      int size = arr.length;
+      System.out.println("배열의 크기 : " + size);
+
+      System.out.println("arr[0] = "+arr[0]);
+      System.out.println("arr[1] = "+arr[1]);
+      System.out.println("arr[2] = "+arr[2]);
+      System.out.println("arr[3] = "+arr[3]);
+
+//      for(int i=0; i < size ; i++){
+//         System.out.println("arr["+i+"] = "+arr[i]);
+//      }
+
+   }
+}
---
+++ 2021_2_OOP/3rd/src/exercise07.java
@@ -0,0 +1,21 @@
+public class exercise07 {
+   public static void main(String[] args){
+      double[] arr = new double[4];
+      arr[0] = 1.1;
+      arr[1] = 2.2;
+      arr[2] = 3.3;
+      arr[3] = 4.4;
+
+      System.out.println("배열의 크기 : "+arr.length);
+
+      System.out.println("arr[0] = "+arr[0]);
+      System.out.println("arr[1] = "+arr[1]);
+      System.out.println("arr[2] = "+arr[2]);
+      System.out.println("arr[3] = "+arr[3]);
+
+//      for(int i=0; i < size ; i++){
+//         System.out.println("arr["+i+"] = "+arr[i]);
+//      }
+
+   }
+}
