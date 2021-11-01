e81f329093fe23cacc9f71a7a3c7bc31a7e68a17
SunEom <dkxl0701@gmail.com>
2021-11-01 23:46:12 +0900

    Basic chat

--- nomad_zoom/src/server.js
+++ nomad_zoom/src/server.js
@@ -15,15 +15,17 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const sockets = [];
+
 wss.on('connection', (socket) => {
+  sockets.push(socket);
   console.log('Connected to Browser ✅');
   socket.on('close', () => {
     console.log('Disconnected from the browser ❌');
   });
   socket.on('message', (message) => {
-    console.log(message.toString('utf8'));
+    sockets.forEach((aSocket) => aSocket.send(message.toString('utf-8')));
   });
-  socket.send('hello!!!');
 });
 
 server.listen(3000, handleListen);
