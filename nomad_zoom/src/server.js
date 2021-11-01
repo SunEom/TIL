ad5a73a9887a0ac10f4054f58863d25852a894f8
SunEom <dkxl0701@gmail.com>
2021-11-02 00:11:57 +0900

    Distinguish different type of message

--- nomad_zoom/src/server.js
+++ nomad_zoom/src/server.js
@@ -19,12 +19,21 @@ const sockets = [];
 
 wss.on('connection', (socket) => {
   sockets.push(socket);
+  socket['nickname'] = 'Anon';
   console.log('Connected to Browser ✅');
   socket.on('close', () => {
     console.log('Disconnected from the browser ❌');
   });
-  socket.on('message', (message) => {
-    sockets.forEach((aSocket) => aSocket.send(message.toString('utf-8')));
+  socket.on('message', (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case 'new_message':
+        sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
+        break;
+      case 'nickname':
+        socket['nickname'] = message.payload;
+        break;
+    }
   });
 });
 
