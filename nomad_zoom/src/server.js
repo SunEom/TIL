44c6b44314e0d778799a622ec56eee31673916ab
SunEom <dkxl0701@gmail.com>
2021-11-02 17:30:04 +0900

    Add socketIO Admin-UI

--- nomad_zoom/src/server.js
+++ nomad_zoom/src/server.js
@@ -1,6 +1,7 @@
 import http from 'http';
-import SocketIO from 'socket.io';
+import { Server } from 'socket.io';
 import express from 'express';
+import { instrument } from '@socket.io/admin-ui';
 
 const app = express();
 
@@ -13,7 +14,16 @@ app.get('/*', (req, res) => res.redirect('/'));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const httpServer = http.createServer(app);
-const wsServer = SocketIO(httpServer);
+const wsServer = new Server(httpServer, {
+  cors: {
+    origin: ['https://admin.socket.io'],
+    credentials: true,
+  },
+});
+
+instrument(wsServer, {
+  auth: false,
+});
 
 function publicRooms() {
   const {
@@ -28,6 +38,10 @@ function publicRooms() {
   return publicRooms;
 }
 
+function countRoom(roomName) {
+  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+}
+
 wsServer.on('connection', (socket) => {
   socket['nickname'] = 'Anonymous';
   socket.onAny((event) => {
@@ -36,11 +50,11 @@ wsServer.on('connection', (socket) => {
   socket.on('enter_room', (roomName, done) => {
     socket.join(roomName);
     done();
-    socket.to(roomName).emit('welcome', socket.nickname);
+    socket.to(roomName).emit('welcome', socket.nickname, countRoom(roomName));
     wsServer.sockets.emit('room_change', publicRooms());
   });
   socket.on('disconnecting', () => {
-    socket.rooms.forEach((room) => socket.to(room).emit('bye', socket.nickname));
+    socket.rooms.forEach((room) => socket.to(room).emit('bye', socket.nickname, countRoom(room) - 1));
   });
   socket.on('new_message', (msg, room, done) => {
     socket.to(room).emit('new_message', `${socket.nickname}: ${msg}`);
