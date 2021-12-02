21388d6f551d603e39743ff65ad94b634c1fd273
Suneom <dkxl0701@gmail.com>
2021-12-03 02:25:58 +0900

    2021.12.03.

---
+++ 2021_2_OOP/14th/src/Prac16/Printer.java
@@ -0,0 +1,19 @@
+package Prac16;
+
+public class Printer {
+    public void println(int num){
+        System.out.println(num);
+    }
+    
+    public void println(boolean bol){
+        System.out.println(bol);
+    }
+    
+    public void println(double num){
+        System.out.println(num);
+    }
+    
+    public void println(String str){
+        System.out.println(str);
+    }
+}
---
+++ 2021_2_OOP/14th/src/Prac17/Printer.java
@@ -0,0 +1,19 @@
+package Prac17;
+
+public class Printer {
+    public static void println(int num){
+        System.out.println(num);
+    }
+
+    public static void println(boolean bol){
+        System.out.println(bol);
+    }
+
+    public static void println(double num){
+        System.out.println(num);
+    }
+
+    public static void println(String str){
+        System.out.println(str);
+    }
+}
