36a097352cfbab978cddb36b19305db51e7d2839
Suneom <dkxl0701@gmail.com>
2021-12-22 16:01:38 +0900

    Updated Navbar UI

--- nextjs/nextjs-intro/pages/index.js
+++ nextjs/nextjs-intro/pages/index.js
@@ -1,9 +1,9 @@
-import { useState } from "react";
-import NavBar from "../components/NavBar";
-
+import Head from "next/head";
+import Seo from "../components/Seo";
 export default function Home() {
   return (
     <div>
+      <Seo title="Home" />
       <h1 className="active">hello </h1>
     </div>
   );
