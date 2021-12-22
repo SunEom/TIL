01f9a0998cab9bc1a47cddfa350f3310c5f3df7d
Suneom <dkxl0701@gmail.com>
2021-12-22 16:30:57 +0900

    Hide API keys

--- nextjs/nextjs-intro/next.config.js
+++ nextjs/nextjs-intro/next.config.js
@@ -1,3 +1,32 @@
+const API_KEY = process.env.API_KEY;
 module.exports = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/old-blog/",
+        destination: "https://nomadcoders.co",
+        permanent: false,
+      },
+      {
+        source: "/old-blog/:path",
+        destination: "/new-sext-blog/:path",
+        permanent: false,
+      },
+      {
+        source: "/old-blog/:path*",
+        destination: "/new-sext-blog/:path*",
+        permanent: false,
+      },
+    ];
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
