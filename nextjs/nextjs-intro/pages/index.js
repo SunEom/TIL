1aa5f73a672eef226e8a49c82744adbe7a001ce4
Suneom <dkxl0701@gmail.com>
2021-12-23 17:56:54 +0900

    getServerSideProps

--- nextjs/nextjs-intro/pages/index.js
+++ nextjs/nextjs-intro/pages/index.js
@@ -13,7 +13,7 @@ export default function Home({ results }) {
 
   const router = useRouter();
   const onClick = (id, title) => {
-    router.push({ pathname: `/movies/${id}`, query: { title } }, `/movies/${id}`);
+    router.push(`/movies/${title}/${id}`);
   };
   return (
     <div className="container">
@@ -22,13 +22,7 @@ export default function Home({ results }) {
       {results?.map((movie) => (
         <div className="movie" onClick={() => onClick(movie.id, movie.original_title)} key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-          <Link
-            href={{
-              pathname: `/movies/${movie.id}`,
-              query: { title: movie.original_title },
-            }}
-            as={`/movies/${movie.id}`}
-          >
+          <Link href={`/movies/${movie.original_title}/${movie.id}`}>
             <a>
               <h4>{movie.original_title}</h4>
             </a>
