a8f429445b2bfc986e881b0f574628bed977227d
Suneom <dkxl0701@gmail.com>
2022-01-02 19:14:58 +0900

    2022. 01. 02.

---
+++ switf-intro/Expression.playground/Contents.swift
@@ -0,0 +1,38 @@
+var myResult = 1 + 2
+var x:Int?
+var y = 10
+x = 10
+x = x! + y
+x = y
+
+var z = 3
+
+x = y * 10 + z - 5 / 4
+
+if x == y {
+    // Something
+}
+
+var result : Bool?
+
+
+var flag = true
+var secondFlag = !flag
+
+if(10<20) || (20<10){
+    print("Expression is ture")
+}
+
+if (10<20)&&(20<10){
+    print("Expression is ture")
+}
+
+
+let x1 = 10
+let y1 = 20
+
+print("Largest number is \(x1 > y1 ? x1 : y1)")
+
+let z1 = 3
+print("Result is \(z1)")
+print("Result is \(~z1)")
