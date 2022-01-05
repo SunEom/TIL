a484fff25e698a37fbe2c7f10937616983feed84
Suneom <dkxl0701@gmail.com>
2022-01-05 15:29:31 +0900

    2022.01.05. : SwiftUI Tutorial

---
+++ swift-intro/SwiftUIDemo/SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by 엄태양 on 2022/01/05.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
