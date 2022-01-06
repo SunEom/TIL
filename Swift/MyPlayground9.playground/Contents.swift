9d5ad62f4508a295f8fdfec46b9cc7437ab90a3b
Suneom <dkxl0701@gmail.com>
2022-01-06 17:11:20 +0900

    2022.01.06. : assert & guard

---
+++ Swift/MyPlayground9.playground/Contents.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+var value = 0
+assert(value == 0)
+
+value = 2
+//assert(value == 0, "값이 0이 아닙니다.")
+
+func guardTest(value: Int?) {
+    guard let value = value else { return }
+    print(value)
+}
+
+guardTest(value: 2)
+guardTest(value: nil)
