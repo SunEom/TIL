33f618ae402671a1be97208759e9f3f700dc2035
Suneom <dkxl0701@gmail.com>
2022-01-06 17:26:18 +0900

    2022.01.06. : Extension

---
+++ Swift/MyPlayground11.playground/Contents.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+extension Int {
+    var isEven: Bool {
+        return self % 2 == 0
+    }
+    
+    var isOdd: Bool {
+        return self % 2 == 1
+    }
+}
+
+var number = 3
+
+number.isEven
+number.isOdd
+
+extension String {
+    func convertToInt () -> Int? {
+        return Int(self)
+    }
+}
+
+var string = "0"
+string.convertToInt()
