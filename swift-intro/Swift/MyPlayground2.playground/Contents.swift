a36e2fdd33bc5deeea2c9d534fa93b5a1acc5cc7
Suneom <dkxl0701@gmail.com>
2022-01-06 15:59:17 +0900

    2022.01.06. : 옵셔널 바인딩

--- swift-intro/Swift/MyPlayground2.playground/Contents.swift
+++ swift-intro/Swift/MyPlayground2.playground/Contents.swift
@@ -6,3 +6,35 @@ var optionalName: String? = "Gunter"
 print(optionalName)
 
 //var requiredName: String = optionalName  //Not Allowed
+
+//Optional Binding
+
+var number: Int? = 3
+print(number)
+print(number!) // Dangerous
+
+if let result = number {
+    print(result)
+} else {
+ 
+}
+
+func test() {
+    let number: Int? = 5
+    guard let result = number else { return }
+    print(result)
+}
+
+test()
+
+let value: Int? = 6
+
+if value == 6 {
+    print("value가 6입니다.")
+} else {
+    print("value가 6이 아닙니다.")
+}
+
+let string = "12"
+var stringToInt: Int! = Int(string)
+print(stringToInt + 1)
