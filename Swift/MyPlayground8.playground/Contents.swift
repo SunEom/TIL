843955be20ef55be7d419cdb2e809cfd8f3e340e
Suneom <dkxl0701@gmail.com>
2022-01-06 17:05:19 +0900

    2022.01.06. : 타입 캐스팅

--- Swift/MyPlayground8.playground/Contents.swift
+++ Swift/MyPlayground8.playground/Contents.swift
@@ -1,3 +1,53 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+class MediaItem {
+    var name: String
+    init(name: String){
+        self.name = name
+    }
+}
+
+class Movie: MediaItem {
+    var director: String
+    init(name: String, director: String) {
+        self.director = director
+        super.init(name: name)
+    }
+}
+
+class Song: MediaItem {
+    var artist: String
+    init(name: String, artist: String){
+        self.artist = artist
+        super.init(name: name)
+    }
+}
+
+let library = [
+    Movie(name: "기생충", director: "봉준호"),
+    Song(name: "Butter", artist: "BTS"),
+    Movie(name: "Old Boy", director: "Park"),
+    Song(name: "Wonderwall", artist: "Oasis"),
+    Song(name: "Rain", artist: "Lee juck")
+]
+
+var movieCount = 0
+var songCount = 0
+
+for item in library {
+    if item is Movie {
+        movieCount += 1
+    } else if item is Song {
+        songCount += 1
+    }
+}
+
+print("Media library contains \(movieCount) movies and \(songCount) songs")
+
+for item in library {
+    if let movie = item as? Movie {
+        print("Movie: \(movie.name), dir. \(movie.director)")
+    } else if let song = item as? Song {
+        print("Song: \(song.name), by \(song.artist)")
+    }
+}
