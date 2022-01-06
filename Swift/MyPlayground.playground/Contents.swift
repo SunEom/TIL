96ea350ce222ae486299a53b7fb3644ce751e5a8
Suneom <dkxl0701@gmail.com>
2022-01-06 15:17:09 +0900

    2022.01.06. : 데이터 타입

--- Swift/MyPlayground.playground/Contents.swift
+++ Swift/MyPlayground.playground/Contents.swift
@@ -7,8 +7,34 @@ for i in 0...99{
     i
 }
 
+
+// Variable & Constant
+
 let a: Int = 100
 
 var b: Int = 200
 
-git
+// Data Type
+
+var someInt: Int = -100
+someInt = 100
+
+var someUInt: UInt = 200
+
+var someFloat: Float = 1.1
+someFloat = 1
+print(someFloat)
+
+var someDouble: Double = 1.1
+someDouble = 1
+
+var someBool: Bool = true
+someBool = false
+
+var someCharacter: Character = "가"
+someCharacter = "A"
+someCharacter = "☂️"
+
+var someString: String = "Hello! 😀"
+
+var number = 10 // 타입 추론
