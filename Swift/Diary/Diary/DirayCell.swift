e9ee2d92a9416b3a9768bf89fdfa87d12d8f6963
Suneom <dkxl0701@gmail.com>
2022-01-10 23:17:38 +0900

    2022.01.10. : Diary 1

---
+++ Swift/Diary/Diary/DirayCell.swift
@@ -0,0 +1,12 @@
+//
+//  DirayCell.swift
+//  Diary
+//
+//  Created by 엄태양 on 2022/01/10.
+//
+
+import UIKit
+
+class DirayCell: UICollectionViewCell {
+    
+}
---
+++ Swift/Diary/Diary/StarCell.swift
@@ -0,0 +1,12 @@
+//
+//  StarCell.swift
+//  Diary
+//
+//  Created by 엄태양 on 2022/01/10.
+//
+
+import UIKit
+
+class StarCell: UICollectionViewCell {
+    
+}
