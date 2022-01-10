4de19e6d8111ac31538ce57c2d1a4d94f714f860
Suneom <dkxl0701@gmail.com>
2022-01-10 23:29:21 +0900

    2022.01.10. : Diary 3

---
+++ Swift/Diary/Diary/DiaryDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DiaryDetailViewController.swift
+//  Diary
+//
+//  Created by 엄태양 on 2022/01/10.
+//
+
+import UIKit
+
+class DiaryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
