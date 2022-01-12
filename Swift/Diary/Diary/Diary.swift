4c84368289a5c3fc92e2c2227334f198956195b8
Suneom <dkxl0701@gmail.com>
2022-01-12 16:22:01 +0900

    일기장 기능 구현하기(일기 작성)

---
+++ Swift/Diary/Diary/Diary.swift
@@ -0,0 +1,15 @@
+//
+//  Diary.swift
+//  Diary
+//
+//  Created by 엄태양 on 2022/01/12.
+//
+
+import Foundation
+
+struct Diary {
+    var title: String
+    var contents: String
+    var date: Date
+    var isStar: Bool
+}
