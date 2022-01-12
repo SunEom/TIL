1ba7dcb231bbcd836d6814c588228a99fa3dd045
Suneom <dkxl0701@gmail.com>
2022-01-12 15:22:20 +0900

    기본 UI 그리기 - 01

---
+++ Swift/Diary/Diary/DiaryCell.swift
@@ -0,0 +1,12 @@
+//
+//  DiaryCell.swift
+//  Diary
+//
+//  Created by 엄태양 on 2022/01/12.
+//
+
+import UIKit
+
+class DiaryCell: UICollectionViewCell {
+    
+}
---
+++ Swift/Diary/Diary/StarCell.swift
@@ -0,0 +1,12 @@
+//
+//  StarCell.swift
+//  Diary
+//
+//  Created by 엄태양 on 2022/01/12.
+//
+
+import UIKit
+
+class StarCell: UICollectionViewCell {
+    
+}
