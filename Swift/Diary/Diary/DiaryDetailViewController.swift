11c2493e5792087acde7317a3d2f0e745bc7b83f
Suneom <dkxl0701@gmail.com>
2022-01-12 15:32:40 +0900

    기본 UI 그리기 - 03

---
+++ Swift/Diary/Diary/DiaryDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DiaryDetailViewController.swift
+//  Diary
+//
+//  Created by 엄태양 on 2022/01/12.
+//
+
+import UIKit
+
+class DiaryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
