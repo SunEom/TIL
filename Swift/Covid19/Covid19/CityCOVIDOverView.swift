e79374a2a38b38a4dc932b3cefb6864f748b4db8
Suneom <dkxl0701@gmail.com>
2022-02-10 21:35:58 +0900

    기능 구현하기 -1

--- Swift/Covid19/Covid19/CityCOVIDOverView.swift
+++ Swift/Covid19/Covid19/CityCOVIDOverView.swift
@@ -1,8 +1,41 @@
 //
-//  CityCOVIDOverView.swift
+//  CityCovidOverview.swift
 //  COVID19
 //
 //  Created by 엄태양 on 2022/02/10.
 //
 
 import Foundation
+
+import Foundation
+
+struct CityCovidOverview: Codable {
+	let korea: CovidOverview
+	let seoul: CovidOverview
+	let busan: CovidOverview
+	let daegu: CovidOverview
+	let incheon: CovidOverview
+	let gwangju: CovidOverview
+	let daejeon: CovidOverview
+	let ulsan: CovidOverview
+	let sejong: CovidOverview
+	let gyeonggi: CovidOverview
+	let chungbuk: CovidOverview
+	let chungnam: CovidOverview
+	let jeonbuk: CovidOverview
+	let jeonnam: CovidOverview
+	let gyeongbuk: CovidOverview
+	let gyeongnam: CovidOverview
+	let jeju: CovidOverview
+}
+
+struct CovidOverview: Codable {
+	let countryName: String
+	let newCase: String
+	let totalCase: String
+	let recovered: String
+	let death: String
+	let percentage: String
+	let newCcase: String
+	let newFcase: String
+}
