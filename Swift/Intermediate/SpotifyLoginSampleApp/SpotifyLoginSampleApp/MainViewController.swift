9199708acf6c2bde9b60b488e06faa3985e29593
Suneom <dkxl0701@gmail.com>
2022-02-11 16:50:18 +0900

    이메일,비밀번호 로그아웃

--- Swift/Intermediate/SpotifyLoginSampleApp/SpotifyLoginSampleApp/MainViewController.swift
+++ Swift/Intermediate/SpotifyLoginSampleApp/SpotifyLoginSampleApp/MainViewController.swift
@@ -30,6 +30,13 @@ class MainViewController: UIViewController {
 	}
 
 	@IBAction func logoutButtonTapped(_ sender: Any) {
-		self.navigationController?.popToRootViewController(animated: true)
+		let firebaseAuth = Auth.auth()
+		do {
+			try firebaseAuth.signOut()
+			self.navigationController?.popToRootViewController(animated: true)
+		} catch let signOutError as NSError {
+			print("ERROR: singout \(signOutError.localizedDescription)")
+		}
+		
 	}
 }
