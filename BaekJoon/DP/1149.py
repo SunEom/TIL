111a9ff1d762fd2d923e2cd7ceb9d9d16b5f060a
Suneom <dkxl0701@gmail.com>
2022-02-15 02:36:17 +0900

    2022.02.15. : 백준 알고리즘

--- BaekJoon/DP/1149.py
+++ BaekJoon/DP/1149.py
@@ -1,23 +1,11 @@
 import sys
+read = sys.stdin.readline
 
+N = int(read())
+cache =[list(map(int, read().split())) for _ in range(N)]
 
-T = int(input())
-
-cost = []
-for _ in range(T):
-    R, G, B = map(int, sys.stdin.readline().split())
-    cost.append({"R":R, "G":G, "B":B})
-
-def dp(prev, n, T):    
-    if n == T:
-        return 0
-
-    elif n == 0:
-        temp = list(filter(lambda x: x[0]!= prev , cost[n].items()))
-        return min(temp[0][1] + dp(temp[0][0], n+1, T), temp[1][1] + dp(temp[1][0], n+1, T) , temp[2][1] + dp(temp[2][0], n+1, T))
-
-    else:
-        temp = list(filter(lambda x: x[0]!= prev , cost[n].items()))
-        return min(temp[0][1] + dp(temp[0][0], n+1, T), temp[1][1] + dp(temp[1][0], n+1, T))
-
-print(dp("", 0, T))
+for i in range(1, N):
+    cache[i][0] += min(cache[i-1][1], cache[i-1][2])
+    cache[i][1] += min(cache[i-1][0], cache[i-1][2])
+    cache[i][2] += min(cache[i-1][0], cache[i-1][1])
+print(min(cache[-1]))
