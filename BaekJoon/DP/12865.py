d3975f1d8c33a081536c8dd95ebb38bc9088b92c
Suneom <dkxl0701@gmail.com>
2022-02-19 12:15:30 +0900

    2022.02.19. : 백준 알고리즘

--- BaekJoon/DP/12865.py
+++ BaekJoon/DP/12865.py
@@ -1,37 +1,15 @@
 import sys
+read = sys.stdin.readline
 
+word1, word2 = read().strip(), read().strip()
+l1, l2 = len(word1), len(word2)
+cache = [0] * l2
 
-n, k = map(int, sys.stdin.readline().split())
-
-li = []
-
-for _ in range(n):
-    w, v = map(int, sys.stdin.readline().split())
-    li.append((w,v))
-
-li = sorted(li, key = lambda x: x[1]/x[0])
-
-dp = []
-dp2 = [x[0] for x in li]
-
-for x in li:
-    if x[0] <= k:
-        dp.append(x[1])
-    else:
-        dp.append(-1)
-
-for i in range(n):
-    for j in range(i):
-        if li[i][0] + dp2[j] <= k:
-            temp = max(dp[i], dp[j]+li[i][1])
-                
-            if temp == dp[j]+li[i][1]:
-                dp2[i] = li[i][0] + dp2[j]
-
-            if dp[i] == dp[j]+li[i][1]:
-                dp2[i] = min(dp2[i], li[i][0] + dp2[j])
-
-            dp[i] = temp
-            
-print(dp ,dp2)
-print(max(dp))
+for i in range(l1):
+    cnt = 0
+    for j in range(l2):
+        if cnt < cache[j]:
+            cnt = cache[j]
+        elif word1[i] == word2[j]:
+            cache[j] = cnt + 1
+print(max(cache))
