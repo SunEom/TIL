9f11effd5a8e48e1c841bd7b5adbcf53b56004bb
Suneom <dkxl0701@gmail.com>
2022-02-24 15:18:53 +0900

    Swift UI 알아보기

---
+++ Swift/Upper Intermediate/SwiftUIPractice/SwiftUIPractice/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUIPractice
+//
+//  Created by 엄태양 on 2022/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        MyView(helloFont: .title)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift/Upper Intermediate/SwiftUIPractice/SwiftUIPractice/SwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeApp.swift
+//  SwiftUIPractice
+//
+//  Created by 엄태양 on 2022/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
