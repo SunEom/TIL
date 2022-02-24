e389cb0333ea0e8bc6a54b7a8bb053158a26dbcb
Suneom <dkxl0701@gmail.com>
2022-02-24 15:53:44 +0900

    SwiftUI를 이용하여 구현하기

---
+++ Swift/Upper Intermediate/MyAsset/MyAsset/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  MyAsset
+//
+//  Created by 엄태양 on 2022/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection: Tab = .asset
+    
+    enum Tab {
+        case asset
+        case recommand
+        case alarm
+        case setting
+    }
+    
+    var body: some View {
+        TabView(selection: $selection) {
+            Color.white
+                .tabItem {
+                    Image(systemName: "dollarsign.circle.fill")
+                    Text("자산")
+                }
+                .tag(Tab.asset)
+            Color.blue
+                .edgesIgnoringSafeArea(.all)
+                .tabItem {
+                    Image(systemName: "hand.thumbsup.fill")
+                    Text("추천")
+                }
+                .tag(Tab.recommand)
+            Color.yellow
+                .edgesIgnoringSafeArea(.all)
+                .tabItem {
+                    Image(systemName: "bell.fill")
+                    Text("알림")
+                }
+                .tag(Tab.alarm)
+            Color.red
+                .edgesIgnoringSafeArea(.all)
+                .tabItem {
+                    Image(systemName: "gearshape.fill")
+                    Text("설정")
+                }
+                .tag(Tab.setting)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift/Upper Intermediate/MyAsset/MyAsset/MyAssetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAssetApp.swift
+//  MyAsset
+//
+//  Created by 엄태양 on 2022/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MyAssetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
