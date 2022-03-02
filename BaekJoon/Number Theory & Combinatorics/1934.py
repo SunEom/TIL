47d616326618cb2f09cc0db544143155b1b6c45a
Suneom <dkxl0701@gmail.com>
2022-03-02 14:19:58 +0900

    2022.03.02. : 백준 알고리즘

---
+++ BaekJoon/Number Theory & Combinatorics/1934.py
@@ -0,0 +1,30 @@
+import sys
+
+n = int(sys.stdin.readline())
+for _ in range(n):
+    x, y = map(int, sys.stdin.readline().split())
+    a, b = x, y
+
+    gcd = None
+    lcm = None
+
+    while True :
+        if a == 1 or b == 1:
+            gcd = 1
+            break
+        if a > b:
+            if a%b ==0:
+                gcd = b
+                break
+            else:
+                a %= b
+        else:
+            if b%a ==0:
+                gcd = a
+                break
+            else :
+                b %= a
+
+    lcm = x * y // gcd
+
+    print(lcm)
