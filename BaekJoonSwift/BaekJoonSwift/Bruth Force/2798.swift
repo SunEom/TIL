0ffe47e445c02cde6fa4358975718378274fe958
Suneom <dkxl0701@gmail.com>
2022-03-19 11:18:26 +0900

    2022.03.19. : 백준 알고리즘

---
+++ BaekJoonSwift/BaekJoonSwift/Bruth Force/2798.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+let iValue = readLine()!.split(separator: " ").map { Int($0)! }
+let len = iValue[0]
+let goal = iValue[1]
+var max = -1
+let numbers = readLine()!.split(separator: " ").map { Int($0)! }
+
+for i in 0..<len-2 {
+    for j in i+1..<len-1 {
+        for k in j+1..<len {
+            let temp = numbers[i] + numbers[j] + numbers[k]
+            if temp <= goal && temp > max {
+                max = temp
+            }
+        }
+    }
+}
+
+print(max)
