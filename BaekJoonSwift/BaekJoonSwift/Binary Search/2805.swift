77cf189a25801628d7d807ba75c0a694c6b8bfca
Suneom <dkxl0701@gmail.com>
2022-05-24 19:10:06 +0900

    2022.05.24. : 백준 알고리즘

--- BaekJoonSwift/BaekJoonSwift/Binary Search/2805.swift
+++ BaekJoonSwift/BaekJoonSwift/Binary Search/2805.swift
@@ -29,30 +29,3 @@ while s <= e {
     }
 }
 print(result)
-
-
-//var s = 0
-//var e = input[0]-1
-//
-//while s <= e {
-//    let m = (s+e)/2
-//
-//    var temp = 0
-//    for num in arr[m...input[0]-1] {
-//        temp += num-arr[m]
-//    }
-//
-//    if temp == input[1] {
-//        result = arr[m]
-//        break
-//    }
-//    else if temp > input[1] {
-//        result = arr[m] + Int(ceil(Double(temp - input[1]) / Double(input[0]-1-m)))
-//        s = m + 1
-//    }
-//    else if temp < input[1] {
-//        e = m - 1
-//    }
-//}
-//
-//print(result)
