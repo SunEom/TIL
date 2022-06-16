2ccb807d0af62be8062e46b15c6dae23329d3ac0
SunEom <dkxl0701@gmail.com>
2022-06-17 00:26:35 +0900

    2022.06.17. : 백준 알고리즘

--- BaekJoonSwift/BaekJoonSwift/Graph/24445.swift
+++ BaekJoonSwift/BaekJoonSwift/Graph/24445.swift
@@ -1,33 +1,35 @@
 import Foundation
 
 let input = readLine()!.split(separator: " ").map { Int($0)! }
-let N = input[0] // 노드의 개수
-let M = input[1] // 간선의 개수
+let N = input[0]
+let M = input[1]
 let R = input[2]
-
 var order = 1
-var edges = Array(repeating: [Int](), count: N+1)
-var visited = Array(repeating: 0, count: N+1)
 var result = ""
 
+var queue = [Int]()
+var visited = Array(repeating: 0, count: N+1)
+var edges =  Array(repeating: [Int](), count: N+1)
+
+
 for _ in 0..<M {
-    let temp = readLine()!.split(separator: " ").map { Int($0)! }
+    let temp = readLine()!.split(separator: " ").map { Int(String($0))! }
     edges[temp[0]].append(temp[1])
     edges[temp[1]].append(temp[0])
 }
 
 for i in 1...N {
-    edges[i].sort(by: > )
+    edges[i].sort(by: >)
 }
 
 func bfs(_ r: Int) {
-    var queue = [Int]()
     visited[r] = order
-    queue.append(r)
     order += 1
-    
-    while !queue.isEmpty {
-        let temp = queue.removeFirst()
+    queue.append(r)
+    var idx = 0
+    while idx < queue.count {
+        let temp = queue[idx]
+        idx += 1
         for v in edges[temp] {
             if visited[v] == 0 {
                 queue.append(v)
@@ -40,8 +42,8 @@ func bfs(_ r: Int) {
 
 bfs(R)
 
-for o in visited[1...] {
-    result += "\(o)\n"
+for i in 1...N {
+    result += "\(visited[i])\n"
 }
 
 print(result)
