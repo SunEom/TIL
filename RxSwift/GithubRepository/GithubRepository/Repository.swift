0234578138f76558c72da31cd763e5f1e5bf9af8
SunEom <dkxl0701@gmail.com>
2022-07-27 20:36:19 +0900

    RxSwift

---
+++ RxSwift/GithubRepository/GithubRepository/Repository.swift
@@ -0,0 +1,21 @@
+//
+//  Repository.swift
+//  GithubRepository
+//
+//  Created by 엄태양 on 2022/07/27.
+//
+
+import Foundation
+
+struct Repository: Decodable {
+    let id: Int
+    let name: String
+    let description: String
+    let stargazersCount: Int
+    let language: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name, description, language
+        case stargazersCount = "stargazers_count"
+    }
+}
