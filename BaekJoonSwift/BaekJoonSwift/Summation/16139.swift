36cc7f0bd8671187b72351b80adfafd22cf2d3ea
SunEom <dkxl0701@gmail.com>
2022-10-21 22:44:39 +0900

    2022.10.21. 백준 알고리즘

--- BaekJoonSwift/BaekJoonSwift/Summation/16139.swift
+++ BaekJoonSwift/BaekJoonSwift/Summation/16139.swift
@@ -1,18 +1,42 @@
 import Foundation
 
-let string = Array(readLine()!)
-let count = Int(readLine()!)!
+let str = Array(readLine()!)
+let len = str.count
+var count = [Character: [Int]]()
+var index = [Character: Int]()
 var result = ""
 
-for _ in 0..<count {
-    let input = readLine()!.split(separator: " ")
-    let ch = Character(String(input[0]))
-    let s = Int(input[1])!
-    let e = Int(input[2])!
-    let temp = string[s...e].filter { c in
-        return c == ch
-    }.count
-    result += "\(temp)\n"
+for i in Character("a").asciiValue!...Character("z").asciiValue! {
+    var temp = Array(repeating: 0, count: len)
+    if str[0].asciiValue! == i {
+        temp[0] = 1
+    }
+    
+    for j in 1..<len {
+        if Character(String(str[j])).asciiValue! == i {
+            temp[j] = temp[j-1] + 1
+        } else {
+            temp[j] = temp[j-1]
+        }
+    }
+    
+    count[Character(String(UnicodeScalar(i)))] = temp
+    
 }
 
+let c = Int(readLine()!)!
+
+for _ in 0..<c {
+    let input = readLine()!.split(separator: " ")
+    let char = Character(String(input[0]))
+    let start = Int(input[1])!
+    let end = Int(input[2])!
+    
+    if start == 0 {
+        result += "\(count[char]![end])\n"
+    } else {
+        result += "\(count[char]![end] - count[char]![start-1])\n"
+    }
+    
+}
 print(result)
