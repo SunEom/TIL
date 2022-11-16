c93ad4d504b56e375de4331813c0f47cfefffa3f
SunEom <dkxl0701@gmail.com>
2022-11-17 01:08:49 +0900

    2022.11.17. 백준 알고리즘

--- BaekJoonSwift/BaekJoonSwift/Geometry/main.swift
+++ BaekJoonSwift/BaekJoonSwift/Geometry/main.swift
@@ -1,16 +1,29 @@
-// CCW
+// 선분 교차 1
 
-let p1 = readLine()!.split(separator: " ").map { Int(String($0))! }
-let p2 = readLine()!.split(separator: " ").map { Int(String($0))! }
-let p3 = readLine()!.split(separator: " ").map { Int(String($0))! }
+import Foundation
 
-let outerProduct = (p1[0]*p2[1] + p2[0]*p3[1] + p3[0]*p1[1]) - (p1[1]*p2[0] + p2[1]*p3[0] + p3[1]*p1[0])
+func ccw(_ x1: Int,_ y1: Int,_ x2: Int,_ y2: Int,_ x3: Int,_ y3: Int) -> Int {
+    if ((x1*y2+x2*y3+x3*y1) - (y1*x2+y2*x3+y3*x1)) < 0 {
+        return -1
+    } else {
+        return 1
+    }
+}
 
-if outerProduct == 0 {
-    print(0)
-} else if outerProduct > 0 {
+let input1 = readLine()!.split(separator: " ").map { Int(String($0))! }
+let input2 = readLine()!.split(separator: " ").map { Int(String($0))! }
+
+let x1 = input1[0], y1 = input1[1], x2 = input1[2], y2 = input1[3]
+let x3 = input2[0], y3 = input2[1], x4 = input2[2], y4 = input2[3]
+
+let a123 = ccw(x1, y1, x2, y2, x3, y3)
+let a124 = ccw(x1, y1, x2, y2, x4, y4)
+let a341 = ccw(x3, y3, x4, y4, x1, y1)
+let a342 = ccw(x3, y3, x4, y4, x2, y2)
+
+if a123*a124 < 0 && a341*a342 < 0 {
     print(1)
 } else {
-    print(-1)
+    print(0)
 }
 
