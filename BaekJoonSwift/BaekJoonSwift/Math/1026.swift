ed90e92c622376a94382cae7100cde3839b21a41
SunEom <dkxl0701@gmail.com>
2023-01-20 23:18:02 +0900

    2023.01.21. 백준 알고리즘

---
+++ BaekJoonSwift/BaekJoonSwift/Math/1026.swift
@@ -0,0 +1,17 @@
+// 1026   보물
+
+import Foundation
+
+let N = Int(readLine()!)!
+var arr1 = readLine()!.split(separator: " ").map{ Int(String($0))! }
+var arr2 = readLine()!.split(separator: " ").map{ Int(String($0))! }
+var result = 0
+
+arr1.sort(by: <)
+arr2.sort(by: >)
+
+zip(arr1, arr2).forEach { (a,b) in
+    result += a*b
+}
+
+print(result)
