f20e4227fb96451af5fdcadd5d921e88af25f725
SunEom <dkxl0701@gmail.com>
2023-02-09 18:54:51 +0900

    2023.02.09. 백준 알고리즘

---
+++ BaekJoonSwift/BaekJoonSwift/Binary Search/main.swift
@@ -0,0 +1,22 @@
+// 1365 꼬인 전깃줄
+
+import Foundation
+
+let N = Int(readLine()!)!
+let arr = readLine()!.split(separator: " ").map { Int(String($0))! }
+var queue = [arr[0]]
+
+for i in 1..<arr.count {
+    if queue.last! <= arr[i] {
+        queue.append(arr[i])
+    } else {
+        for j in 0..<queue.count {
+            if queue[j] >= arr[i] {
+                queue[j] = arr[i]
+                break
+            }
+        }
+    }
+}
+
+print(N - queue.count)
