bad27db5d93cf256c441d9bd3a5b9f8249ccfc45
SunEom <dkxl0701@gmail.com>
2023-08-13 19:30:52 +0900

    2023.08.13. 백준 알고리즘

--- BaekJoonSwift/BaekJoonSwift/DP (2)/11727.swift
+++ BaekJoonSwift/BaekJoonSwift/DP (2)/11727.swift
@@ -1,11 +1,12 @@
-// 2xn 타일링 2
+// 11727 2xM 타일링 2
 
-var dp = [0, 1, 3]
+var dp = [0,1,3]
 
 let n = Int(readLine()!)!
 
-while dp.count < n + 1 {
-    dp.append((dp[dp.count-1] + dp[dp.count-2]*2)%10007)
+while dp.count <= n {
+    let lastIdx = dp.count-1
+    dp.append((dp[lastIdx-1]*2+dp[lastIdx-0])%10007)
 }
 
 print(dp[n])
--- BaekJoonSwift/BaekJoonSwift/DP (2)/main.swift
+++
@@ -1,8 +0,0 @@
-//
-//  main.swift
-//  BaekJoonSwift
-//
-//  Created by 엄태양 on 2023/08/12.
-//
-
-import Foundation
